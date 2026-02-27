2a740b11f6c1a4d63a4b3a5c3443ed78c7906fc5
katp1998 <katp1998is@gmail.com>
2026-02-27 08:29:18 +0000

    twoSum question completed
    Signed-off-by: katp1998 <katp1998is@gmail.com>

--- typescript/arrays-hashes.ts
+++ typescript/arrays-hashes.ts
@@ -57,7 +57,56 @@ class Solution {
     s and t consist of lowercase English letters.
     */
    isAnagram(s: string, t: string): boolean {
-        return true;    
+
+    return true; 
+   }
+
+   /**
+    * 
+    * 3. Two Sum
+    Given an array of integers nums and an integer target, return the indices i and j such that nums[i] + nums[j] == target and i != j.
+
+    You may assume that every input has exactly one pair of indices i and j that satisfy the condition.
+
+    Return the answer with the smaller index first.
+
+    Example 1:
+
+    Input: 
+    nums = [3,4,5,6], target = 7
+    Output: [0,1]
+
+    Explanation: nums[0] + nums[1] == 7, so we return [0, 1].
+
+    Example 2:
+
+    Input: nums = [4,5,6], target = 10
+    Output: [0,2]
+
+    Example 3:
+
+    Input: nums = [5,5], target = 10
+    Output: [0,1]
+
+    Constraints:
+
+    2 <= nums.length <= 1000
+    -10,000,000 <= nums[i] <= 10,000,000
+    -10,000,000 <= target <= 10,000,000
+    Only one valid answer exists.
+    * 
+    */
+   twoSum(nums:number[], target: number): number[] {
+        let arr: number[] = [];
+        for(let i = 0; i<nums.length; i++){
+            for (let j = i+1; j<nums.length; j++){
+                if(nums[i]+nums[j] == target){
+                    arr.push(i,j);
+                }
+                arr.sort();
+            }
+        }
+        return arr;
    }
 }
 
