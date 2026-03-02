228053b0b43386996f08901f3f89a7ffa4dbbff0
katp1998 <katp1998is@gmail.com>
2026-03-02 23:14:09 +0000

    adding queues
    Signed-off-by: katp1998 <katp1998is@gmail.com>

---
+++ javascript/queue.js
@@ -0,0 +1,58 @@
+
+Queue = () => {
+    collection = []
+    this.print = () => {
+        console.log(collection);
+    }
+
+    this.enqueue = (element) => {
+        collection.push(element)
+    }
+
+    this.dequeue = () => {
+        return collection.shift();
+    }
+
+    this.front = () => {
+        return collection[0];
+    }
+
+    this.size = () => {
+        return collection.length;
+    }
+
+    this.isEmpty = () => {
+        return collection.length === 0;
+    }
+}
+
+
+PriorityQueue = () => {
+    let collection = [];
+    this.printCollection = () => {
+        console.log(collection);
+    }
+
+    this.enqueue = (element) => {
+        if (this.isEmpty()){
+            collection.push(element)
+        } else {
+            let added = false;
+            for (let i = 0; i < collection.length; i++){
+                if(element[i] < collection[i][1]){
+                    collection.splice(i, 0, element);
+                    added = true;
+                    break;
+                }
+            }
+            if(!added){
+                collection.push(element);
+            }
+        }
+    }
+
+    this.dequeue = () => {
+        let value = collection.shift();
+        return value[0];
+    }
+}
